37f3bfc51cbadf31587845e8ff05be8a6eaeb1b5
Phuthana Ampunant <poohpooh4000@hotmail.com>
2022-04-03 11:03:19 +0700

    Changes the table name from product to products

--- nodejslogin/web_project.sql
+++ nodejslogin/web_project.sql
@@ -24,24 +24,25 @@ INSERT INTO Users VALUES
 INSERT INTO Admin VALUES
 (1, "admin", "admin", "beluga@gmail.com");
 
-CREATE TABLE `Product`(
-	Product_ID	char(32) NOT NULL,
-	Product_name varchar(32) NOT NULL,	
-	Rating	decimal(2,1) NOT NULL,	
-	Age_Restriction	int,	
-	Company_name	varchar(32) NOT NULL ,	
-	Product_Price	int	NOT NULL,
-	Product_type	varchar(32) NOT NULL,
-	Product_spec	varchar(32) NOT NULL
+CREATE TABLE `products`(
+	p_id			int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	p_name 			varchar(32) NOT NULL,	
+	rating		decimal(2,1) NOT NULL,	
+	age_restriction	int,	
+	company_name	varchar(32) NOT NULL ,	
+	p_price			int	NOT NULL,
+	p_type			varchar(32) NOT NULL,
+	p_platform		varchar(32) NOT NULL
 );
 
-INSERT INTO `Product` (`Product_ID`, `Product_name`, `Rating`, `Age_Restriction`, `Company_name`, `Product_Price`, `Product_type`, `Product_Spec`) VALUES
-('G112350', 'Succubus', 4.1, 18 ,'MadHouse_studio', 250,'Blood','Console'),
-('G205443', 'Total War WarhammerII', 4.7, 18 ,'SEGA', 1200,'Fighting','Console'),
-('G330123', 'Heros Hours', 3.9, 12 ,'Indie_Software', 90, 'Fighting','Moblie'),
-('G220149', 'Sid Meier s Civilization VI',4.6, 12, '2K_studio',300, 'Turn-base', 'Console'),
-('S996535', 'Pixel Game Maker MV', 4.2, 9,  'KADOKAWA', 1200, 'RPG','Console'),
-('S456888', 'Boba App', 3.1, 5,  'KADOKAWA', 150, 'RPG','Console');
+INSERT INTO `products` (`p_id`, `p_name`, `rating`, `age_restriction`, `company_name`, `p_price`, `p_type`, `p_platform`) VALUES
+(1, 'Succubus', 4.1, 18 ,'MadHouse_studio', 250,'Blood','Console'),
+(2, 'Total War WarhammerII', 4.7, 18 ,'SEGA', 1200,'Fighting','Console'),
+(3, 'Heros Hours', 3.9, 12 ,'Indie_Software', 90, 'Fighting','Moblie'),
+(4, 'Sid Meier s Civilization VI',4.6, 12, '2K_studio',300, 'Turn-base', 'Console'),
+(5, 'Pixel Game Maker MV', 4.2, 9,  'KADOKAWA', 1200, 'RPG','Console'),
+(6, 'Boba App', 3.1, 5,  'KADOKAWA', 150, 'RPG','Console');
+
 -- New
 -- ('S978565', 'Movavi Video Editor', 3.0, 9,  'Movavi Software', 870, 'RPG','Console'),
 -- ('G756565', 'Battlefield 2042', 5.0 , 18,  'DICE', 1599, 'Sadistic','Console'),
@@ -50,5 +51,5 @@ INSERT INTO `Product` (`Product_ID`, `Product_name`, `Rating`, `Age_Restriction`
 -- ('S485234', 'Visual Novel Maker', 1.2 , 6,  'Degica', 699, 'Puzzle','Console');
 
 
-SELECT * FROM Users;
-SELECT * FROM Product;
+-- SELECT * FROM Users;
+SELECT * FROM products;
